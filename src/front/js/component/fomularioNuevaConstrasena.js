ed183331e3df38591225efadd2d80b0ac738805d
yancarq <yancarlosrq@gmail.com>
2021-04-23 07:20:59 +0000

    Estructura de restabelcer 2

--- src/front/js/component/fomularioNuevaConstrasena.js
+++ src/front/js/component/fomularioNuevaConstrasena.js
@@ -5,41 +5,37 @@ import "../../styles/demo.scss";
 
 export const NuevaContrasena = props => {
 	return (
-		<div className="container m-auto col-4">
+		<div className="container m-auto col-lg-4 col-md-6 col-sm-12">
 			<div className="card login-form">
-				<div className="card-body">
+				<div className="card-body col-12 m-auto">
 					<h3 className="card-title text-center">Restablecer su contrase&ntilde;a</h3>
 
 					<div className="card-text">
 						<form>
-							<div className="col-7 m-auto">
-								<div className="form-group text-center m-auto col-10">
-									<label htmlFor="exampleInputEmail1">
-										*** La contrase&ntilde;a debe contener al menos 6 caracteres.
-									</label>
-								</div>
-								<div className="form-group">
-									<input
-										type="password"
-										className="form-control form-control-sm font-weight-bolder"
-										placeholder="Ingrese la nueva contraseña"
-										style={{ fontSize: "1.0rem" }}
-									/>
-								</div>
-								<div className="form-group my-3">
-									<input
-										type="password"
-										className="form-control form-control-sm font-weight-bolder"
-										placeholder="Ingrese nuevamente la contraseña"
-										style={{ fontSize: "1.0rem" }}
-									/>
-								</div>
-								<Link to="/">
-									<button type="submit" className="btn btn-primary btn-block">
-										Establecer nueva contrase&ntilde;a
-									</button>
-								</Link>
+							<div className="alert alert-warning" role="alert">
+								La contrase&ntilde;a debe contener al menos 6 caracteres.
 							</div>
+							<div className="form-group">
+								<input
+									type="password"
+									className="form-control form-control-sm font-weight-bolder"
+									placeholder="Ingrese la nueva contraseña"
+									style={{ fontSize: "1.0rem" }}
+								/>
+							</div>
+							<div className="form-group my-3">
+								<input
+									type="password"
+									className="form-control form-control-sm font-weight-bolder"
+									placeholder="Ingrese nuevamente la contraseña"
+									style={{ fontSize: "1.0rem" }}
+								/>
+							</div>
+							<Link to="/">
+								<button type="submit" className="btn btn-primary btn-block">
+									Establecer nueva contrase&ntilde;a
+								</button>
+							</Link>
 						</form>
 					</div>
 				</div>
