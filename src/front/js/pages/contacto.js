00f410b12908ecbdcd5860d7635c0659ae529379
toxigeno <melissaaraya@yahoo.com>
2021-04-29 20:44:22 +0000

    pag de contacto terminada

--- src/front/js/pages/contacto.js
+++ src/front/js/pages/contacto.js
@@ -4,16 +4,16 @@ const Contacto = () => {
 	return (
 		<div className="my-3 container">
 			<div className="row justify-content-center">
-				<div className="col-md-6 text-center naranja-bg">
-					<h1>Contáctenos</h1>
-					<p>
-						<i className="fas fa-envelope fa-2x" /> contacto@minervabusca.com
+				<div className="col-sm-12 col-lg-7 crema-f-bg borde-crema rounded15 p-4">
+					<h3 className="text-center naranja text-uppercase">Contáctenos</h3>
+					<p className="d-flex align-items-center">
+						<i className="fas fa-envelope fs-9 naranja p-3 text-break" /> contacto@minervabusca.com
 					</p>
-					<p>
-						<i className="flip-hor fab fa fa-phone fa-2x" /> (506) 8888-9999, (506) 7777-2222
+					<p className="d-flex align-items-center">
+						<i className="flip-hor fab fa fa-phone fs-9 naranja p-3" /> (506) 8888-9999, (506) 7777-2222
 					</p>
-					<p>
-						<i className="fa fa-map-marker fa-2x" /> Avenida 5, Calle 2, San José, Costa Rica
+					<p className="d-flex align-items-center">
+						<i className="fa fa-map-marker fs-9 naranja p-3" /> Avenida 5, Calle 2, San José, Costa Rica
 					</p>
 				</div>
 			</div>
