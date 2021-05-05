746be6cdcf6447c15727e4422826f5edb442cf7b
toxigeno <melissaaraya@yahoo.com>
2021-05-05 04:18:09 +0000

    prueba con imagen de fondo para portada

--- src/front/js/component/cartilla.js
+++ src/front/js/component/cartilla.js
@@ -1,9 +1,15 @@
 import React from "react";
+import orwell from "../../img/orwell.jpg";
 
 const Cartilla = () => {
 	return (
 		<div className="m-auto container">
-			<h1>Componente Cartilla</h1>
+			<div className="row my-2">
+				<div className="col-sm-12 p-0 col-md-4 border border-primary">
+					<div className="portada portada-img" style={{ backgroundImage: `url(${orwell})` }} />
+				</div>
+				<div className="col-sm-12 col-md-8 border border-danger">2</div>
+			</div>
 		</div>
 	);
 };
